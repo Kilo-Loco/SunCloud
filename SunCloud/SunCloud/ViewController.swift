776c9e0d9ec8acd881c406b9a6098b27dc6b0c1e
Kyle Lee <kiloloco@me.com>
2018-06-15 20:43:23 -0700

    first commit

---
+++ SunCloud/SunCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunCloud
+//
+//  Created by Kyle Lee on 6/15/18.
+//  Copyright © 2018 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
